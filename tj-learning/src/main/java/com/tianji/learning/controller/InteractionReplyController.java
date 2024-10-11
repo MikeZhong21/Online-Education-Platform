17542e3ab830dd9dd817a4a3ec87ea528fb0a6e4
MikeZhong21 <1151822171@qq.com>
2024-10-11 14:40:17 +0800

    comment features implementation

--- tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
+++ tj-learning/src/main/java/com/tianji/learning/controller/InteractionReplyController.java
@@ -1,9 +1,16 @@
 package com.tianji.learning.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.tianji.common.domain.dto.PageDTO;
+import com.tianji.learning.domain.dto.ReplyDTO;
+import com.tianji.learning.domain.query.ReplyPageQuery;
+import com.tianji.learning.domain.vo.ReplyVO;
+import com.tianji.learning.service.IInteractionReplyService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -14,7 +21,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-10-05
  */
 @RestController
-@RequestMapping("/interaction-reply")
+@Api(tags = "互动问答回复相关接口")
+@RequestMapping("/replies")
+@RequiredArgsConstructor
 public class InteractionReplyController {
+    private final IInteractionReplyService interactionReplyService;
+    @ApiOperation("Submit reply or comment")
+    @PostMapping
+    public void addNewComment(@RequestBody @Validated ReplyDTO replyDTO){
+        interactionReplyService.addNewComment(replyDTO);
+    }
 
+    @ApiOperation("Page query reply and comment list")
+    @GetMapping("/page")
+    public PageDTO<ReplyVO> queryComment(ReplyPageQuery replyPageQuery){
+        return interactionReplyService.queryComment(replyPageQuery);
+    }
 }
--- tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionReplyServiceImpl.java
+++ tj-learning/src/main/java/com/tianji/learning/service/impl/InteractionReplyServiceImpl.java
@@ -1,10 +1,33 @@
 package com.tianji.learning.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.OrderItem;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tianji.api.client.user.UserClient;
+import com.tianji.api.dto.user.UserDTO;
+import com.tianji.common.domain.dto.PageDTO;
+import com.tianji.common.exceptions.BadRequestException;
+import com.tianji.common.utils.BeanUtils;
+import com.tianji.common.utils.CollUtils;
+import com.tianji.common.utils.UserContext;
+import com.tianji.learning.domain.dto.ReplyDTO;
+import com.tianji.learning.domain.po.InteractionQuestion;
 import com.tianji.learning.domain.po.InteractionReply;
+import com.tianji.learning.domain.query.ReplyPageQuery;
+import com.tianji.learning.domain.vo.ReplyVO;
+import com.tianji.learning.enums.QuestionStatus;
 import com.tianji.learning.mapper.InteractionReplyMapper;
+import com.tianji.learning.service.IInteractionQuestionService;
 import com.tianji.learning.service.IInteractionReplyService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static com.tianji.common.constants.Constant.DATA_FIELD_NAME_CREATE_TIME;
+import static com.tianji.common.constants.Constant.DATA_FIELD_NAME_LIKED_TIME;
 
 /**
  * <p>
@@ -15,6 +38,116 @@ import org.springframework.stereotype.Service;
  * @since 2024-10-05
  */
 @Service
+@RequiredArgsConstructor
 public class InteractionReplyServiceImpl extends ServiceImpl<InteractionReplyMapper, InteractionReply> implements IInteractionReplyService {
+    private final UserClient userClient;
+    private final IInteractionQuestionService interactionQuestionService;
+
+    @Override
+    public void addNewComment(ReplyDTO replyDTO) {
+        //获取用户id
+        Long userId = UserContext.getUser();
+        //如果是回答就只需要问题id 回答内容 是否匿名 如果是评论则所有属性都需要
+        InteractionReply reply = BeanUtils.copyBean(replyDTO, InteractionReply.class);
+        reply.setUserId(userId);
+        this.save(reply);
+        boolean isAnswer = replyDTO.getAnswerId() == null;
+        if (!isAnswer) {
+            // 3.2.是评论，则需要更新上级回答的评论数量
+            lambdaUpdate()
+                    .setSql("reply_times = reply_times + 1")
+                    .eq(InteractionReply::getId, replyDTO.getAnswerId())
+                    .update();
+        }
+        // 3.3.尝试更新问题表中的状态、 最近一次回答、回答数量
+        interactionQuestionService.lambdaUpdate()
+                .set(isAnswer, InteractionQuestion::getLatestAnswerId, reply.getAnswerId())
+                .setSql(isAnswer, "answer_times = answer_times + 1")
+                .set(replyDTO.getIsStudent(), InteractionQuestion::getStatus, QuestionStatus.UN_CHECK.getValue())
+                .eq(InteractionQuestion::getId, replyDTO.getQuestionId())
+                .update();
+
+        //todo 如果是学生 则增加积分
+    }
+
+
+    @Override
+    public PageDTO<ReplyVO> queryComment(ReplyPageQuery query) {
+        // 1.问题id和回答id至少要有一个，先做参数判断
+        Long questionId = query.getQuestionId();
+        Long answerId = query.getAnswerId();
+        if (questionId == null && answerId == null) {
+            throw new BadRequestException("问题或回答id不能都为空");
+        }
+        // 标记当前是查询问题下的回答
+        boolean isQueryAnswer = questionId != null;
+        // 2.分页查询reply
+        Page<InteractionReply> page = lambdaQuery()
+                .eq(isQueryAnswer, InteractionReply::getQuestionId, questionId)
+                .eq(InteractionReply::getAnswerId, isQueryAnswer ? 0L : answerId)
+                .eq(InteractionReply::getHidden, false)
+                .page(query.toMpPage( // 先根据点赞数排序，点赞数相同，再按照创建时间排序
+                        new OrderItem(DATA_FIELD_NAME_LIKED_TIME, false),
+                        new OrderItem(DATA_FIELD_NAME_CREATE_TIME, true))
+                );
+        List<InteractionReply> records = page.getRecords();
+        if (CollUtils.isEmpty(records)) {
+            return PageDTO.empty(page);
+        }
+        // 3.数据处理，需要查询：提问者信息、回复目标信息、当前用户是否点赞
+        Set<Long> userIds = new HashSet<>();
+        Set<Long> answerIds = new HashSet<>();
+        Set<Long> targetReplyIds = new HashSet<>();
+        // 3.1.获取提问者id 、回复的目标id、当前回答或评论id（统计点赞信息）
+        for (InteractionReply r : records) {
+            if(!r.getAnonymity()) {
+                // 非匿名
+                userIds.add(r.getUserId());
+            }
+            targetReplyIds.add(r.getTargetReplyId());
+            answerIds.add(r.getId());
+        }
+        // 3.2.查询目标回复，如果目标回复不是匿名，则需要查询出目标回复的用户信息
+        targetReplyIds.remove(0L);
+        targetReplyIds.remove(null);
+        if(targetReplyIds.size() > 0) {
+            List<InteractionReply> targetReplies = listByIds(targetReplyIds);
+            Set<Long> targetUserIds = targetReplies.stream()
+                    .filter(Predicate.not(InteractionReply::getAnonymity))
+                    .map(InteractionReply::getUserId)
+                    .collect(Collectors.toSet());
+            userIds.addAll(targetUserIds);
+        }
+        // 3.3.查询用户
+        Map<Long, UserDTO> userMap = new HashMap<>(userIds.size());
+        if(userIds.size() > 0) {
+            List<UserDTO> users = userClient.queryUserByIds(userIds);
+            userMap = users.stream().collect(Collectors.toMap(UserDTO::getId, u -> u));
+        }
+        // 4.处理VO
+        List<ReplyVO> list = new ArrayList<>(records.size());
+        for (InteractionReply r : records) {
+            // 4.1.拷贝基础属性
+            ReplyVO v = BeanUtils.toBean(r, ReplyVO.class);
+            list.add(v);
+            // 4.2.回复人信息
+            if(!r.getAnonymity()){
+                UserDTO userDTO = userMap.get(r.getUserId());
+                if (userDTO != null) {
+                    v.setUserIcon(userDTO.getIcon());
+                    v.setUserName(userDTO.getName());
+                    v.setUserType(userDTO.getType());
+                }
+            }
+            // 4.3.如果存在评论的目标，则需要设置目标用户信息
+            if(r.getTargetReplyId() != null) {
+                UserDTO targetUser = userMap.get(r.getTargetUserId());
+                if (targetUser != null) {
+                    v.setTargetUserName(targetUser.getName());
+                }
+            }
+        }
+        return new PageDTO<>(page.getTotal(), page.getPages(), list);
+    }
 
 }
