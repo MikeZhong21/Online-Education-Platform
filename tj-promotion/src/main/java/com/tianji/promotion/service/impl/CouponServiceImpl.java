752dc2515c6dadf80cc07ab655165033271b1fa1
MikeZhong21 <1151822171@qq.com>
2024-12-02 23:45:03 +0800

    page query coupon implementation

--- tj-promotion/src/main/java/com/tianji/promotion/service/impl/CouponServiceImpl.java
+++ tj-promotion/src/main/java/com/tianji/promotion/service/impl/CouponServiceImpl.java
@@ -1,11 +1,16 @@
 package com.tianji.promotion.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.tianji.common.domain.dto.PageDTO;
 import com.tianji.common.exceptions.BadRequestException;
 import com.tianji.common.utils.BeanUtils;
 import com.tianji.common.utils.CollUtils;
+import com.tianji.common.utils.StringUtils;
 import com.tianji.promotion.domain.dto.CouponFormDTO;
 import com.tianji.promotion.domain.po.Coupon;
 import com.tianji.promotion.domain.po.CouponScope;
+import com.tianji.promotion.domain.query.CouponQuery;
+import com.tianji.promotion.domain.vo.CouponPageVO;
 import com.tianji.promotion.mapper.CouponMapper;
 import com.tianji.promotion.service.ICouponScopeService;
 import com.tianji.promotion.service.ICouponService;
@@ -53,5 +58,26 @@ public class CouponServiceImpl extends ServiceImpl<CouponMapper, Coupon> impleme
             csList.add(couponScope);
         }
         couponScopeService.saveBatch(csList);
+    }
+
+    @Override
+    public PageDTO<CouponPageVO> queryCouponByPage(CouponQuery query) {
+        Integer status = query.getStatus();
+        String name = query.getName();
+        Integer type = query.getType();
+        // 1.分页查询
+        Page<Coupon> page = lambdaQuery()
+                .eq(type != null, Coupon::getDiscountType, type)
+                .eq(status != null, Coupon::getStatus, status)
+                .like(StringUtils.isNotBlank(name), Coupon::getName, name)
+                .page(query.toMpPageDefaultSortByCreateTimeDesc());
+        // 2.处理VO
+        List<Coupon> records = page.getRecords();
+        if (CollUtils.isEmpty(records)) {
+            return PageDTO.empty(page);
+        }
+        List<CouponPageVO> list = BeanUtils.copyList(records, CouponPageVO.class);
+        // 3.返回
+        return PageDTO.of(page, list);
     }
 }
