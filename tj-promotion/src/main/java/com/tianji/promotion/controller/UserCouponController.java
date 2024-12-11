1dcd2983c30695459274fbccf8dacf64663d5c95
MikeZhong21 <1151822171@qq.com>
2024-12-11 23:46:14 +0800

    receive coupon implementation

--- tj-promotion/src/main/java/com/tianji/promotion/controller/UserCouponController.java
+++ tj-promotion/src/main/java/com/tianji/promotion/controller/UserCouponController.java
@@ -1,6 +1,12 @@
 package com.tianji.promotion.controller;
 
 
+import com.tianji.promotion.service.IUserCouponService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-12-07
  */
 @RestController
-@RequestMapping("/user-coupon")
+@RequiredArgsConstructor
+@RequestMapping("/user-coupons")
+@Api(tags = "Coupon related interface")
 public class UserCouponController {
 
+    private final IUserCouponService userCouponService;
+
+    @ApiOperation("receiving coupon interface")
+    @PostMapping("/{couponId}/receive")
+    public void receiveCoupon(@PathVariable("couponId") Long couponId){
+        userCouponService.receiveCoupon(couponId);
+    }
 }
--- tj-promotion/src/main/java/com/tianji/promotion/service/impl/UserCouponServiceImpl.java
+++ tj-promotion/src/main/java/com/tianji/promotion/service/impl/UserCouponServiceImpl.java
@@ -1,10 +1,18 @@
 package com.tianji.promotion.service.impl;
 
+import com.tianji.common.exceptions.BadRequestException;
+import com.tianji.common.utils.UserContext;
+import com.tianji.promotion.domain.po.Coupon;
 import com.tianji.promotion.domain.po.UserCoupon;
+import com.tianji.promotion.mapper.CouponMapper;
 import com.tianji.promotion.mapper.UserCouponMapper;
 import com.tianji.promotion.service.IUserCouponService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -15,6 +23,59 @@ import org.springframework.stereotype.Service;
  * @since 2024-12-07
  */
 @Service
+@RequiredArgsConstructor
 public class UserCouponServiceImpl extends ServiceImpl<UserCouponMapper, UserCoupon> implements IUserCouponService {
 
+    private final CouponMapper couponMapper;
+
+    @Override
+    @Transactional
+    public void receiveCoupon(Long couponId) {
+
+        Coupon coupon = couponMapper.selectById(couponId);
+        if (coupon == null) {
+            throw new BadRequestException("优惠券不存在");
+        }
+
+        LocalDateTime now = LocalDateTime.now();
+        if (now.isBefore(coupon.getIssueBeginTime()) || now.isAfter(coupon.getIssueEndTime())) {
+            throw new BadRequestException("优惠券发放已经结束或尚未开始");
+        }
+
+        if (coupon.getIssueNum() >= coupon.getTotalNum()) {
+            throw new BadRequestException("优惠券库存不足");
+        }
+        Long userId = UserContext.getUser();
+
+        Integer count = lambdaQuery()
+                .eq(UserCoupon::getUserId, userId)
+                .eq(UserCoupon::getCouponId, couponId)
+                .count();
+        // 4.2.校验限领数量
+        if(count != null && count >= coupon.getUserLimit()){
+            throw new BadRequestException("超出领取数量");
+        }
+        // 5.更新优惠券的已经发放的数量 + 1
+        couponMapper.incrIssueNum(coupon.getId());
+        // 6.新增一个用户券
+        saveUserCoupon(coupon, userId);
+    }
+
+    private void saveUserCoupon(Coupon coupon, Long userId) {
+        // 1.基本信息
+        UserCoupon uc = new UserCoupon();
+        uc.setUserId(userId);
+        uc.setCouponId(coupon.getId());
+        // 2.有效期信息
+        LocalDateTime termBeginTime = coupon.getTermBeginTime();
+        LocalDateTime termEndTime = coupon.getTermEndTime();
+        if (termBeginTime == null) {
+            termBeginTime = LocalDateTime.now();
+            termEndTime = termBeginTime.plusDays(coupon.getTermDays());
+        }
+        uc.setTermBeginTime(termBeginTime);
+        uc.setTermEndTime(termEndTime);
+        // 3.保存
+        save(uc);
+    }
 }
